040fd5cdbc06440644895d81398cacd8c046d814
Rajkarnikar-unish <ubidy947@gmail.com>
2025-07-26 23:30:15 -0500

    test case for healthchecker

--- src/test/java/com/unishraj/actions/ActionsApplicationTests.java
+++ src/test/java/com/unishraj/actions/ActionsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.unishraj.actions;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ActionsApplicationTests {
 
+	@Autowired
+	private ActionsApplication actionsApplication;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void healthCheckerReturnsOk() {
+		assertEquals("OK", actionsApplication.healthChecker() );
 	}
 
 }
